81badea71b9945b9ec082b334233a5759f8172e4
Nathaniel Bibler <nate@envylabs.com>
2017-08-22 13:35:28 -0400

    Reorganize the specs for doc clarity.

--- spec/faraday/detailed_logger/middleware_spec.rb
+++ spec/faraday/detailed_logger/middleware_spec.rb
@@ -8,108 +8,95 @@ require "stringio"
 RSpec.describe Faraday::DetailedLogger::Middleware do
   TestError = Class.new(StandardError)
 
-  it "defaults to logging to STDOUT" do
-    expect { connection(nil).get("/temaki") }.to output.to_stdout
-  end
+  let(:log) { StringIO.new }
+  let(:logger) { Logger.new(log) }
 
-  it "logs with the configured tags prepended to each line" do
-    logger = Logger.new(log = StringIO.new)
-
-    connection(logger, %w[ebi]).get("/temaki")
-    log.rewind
-    log.readlines.each do |line|
-      expect(line).to match(/: \[ebi\] /)
+  context "by default" do
+    it "logs to STDOUT" do
+      expect { connection(nil).get("/temaki") }.to output.to_stdout
     end
   end
 
-  it "logs the request method at an INFO level" do
-    logger = Logger.new(log = StringIO.new)
-
-    connection(logger).get("/temaki")
-    log.rewind
-    expect(log.read).to match(/\bINFO\b.+\bGET\b/)
+  context "with tags configured" do
+    it "logs prepends the tags to each line" do
+      connection(logger, %w[ebi]).get("/temaki")
+      log.rewind
+      log.readlines.each do |line|
+        expect(line).to match(/: \[ebi\] /)
+      end
+    end
   end
 
-  it "logs the request URI at an INFO level" do
-    logger = Logger.new(log = StringIO.new)
+  context "for the HTTP request-portion" do
+    it "logs request method and URI at an INFO level" do
+      connection(logger).get("/temaki")
+      log.rewind
+      expect(log.read).to match(%r{\bINFO\b.+\bGET http://sushi\.com/temaki\b})
+    end
 
-    connection(logger).get("/temaki")
-    log.rewind
-    expect(log.read).to match(%r{\bINFO\b.+\bhttp://sushi.com/temaki\b})
-  end
-
-  it "logs a cURL-like request package at a DEBUG level" do
-    logger = Logger.new(log = StringIO.new)
-
-    connection(logger).post("/nigirizushi", {
-      "body" => "content"
-    },       user_agent: "Faraday::DetailedLogger")
-    log.rewind
-    curl = <<-CURL.strip
+    it "logs the requested body at a DEBUG level" do
+      connection(logger).post("/nigirizushi", {
+        "body" => "content"
+      },       user_agent: "Faraday::DetailedLogger")
+      log.rewind
+      curl = <<-CURL.strip
 User-Agent: Faraday::DetailedLogger
 Content-Type: application/x-www-form-urlencoded
 
 body=content
 CURL
-    expect(log.read).to match(/\bDEBUG\b.+#{Regexp.escape(curl.inspect)}/)
+      expect(log.read).to match(/\bDEBUG\b.+#{Regexp.escape(curl.inspect)}/)
+    end
   end
 
-  it "logs a 2XX response status code at an INFO level" do
-    logger = Logger.new(log = StringIO.new)
+  context "for the HTTP response-portion" do
+    it "logs a 2XX response status code at an INFO level" do
+      connection(logger).get("/oaiso", c: 200)
+      log.rewind
+      expect(log.read).to match(/\bINFO\b.+\bHTTP 200\b/)
+    end
 
-    connection(logger).get("/oaiso", c: 200)
-    log.rewind
-    expect(log.read).to match(/\bINFO\b.+\bHTTP 200\b/)
-  end
+    it "logs a 3XX response status code at an INFO level" do
+      connection(logger).get("/oaiso", c: 301)
+      log.rewind
+      expect(log.read).to match(/\bINFO\b.+\bHTTP 301\b/)
+    end
 
-  it "logs a 3XX response status code at an INFO level" do
-    logger = Logger.new(log = StringIO.new)
+    it "logs a 4XX response status code at a WARN level" do
+      connection(logger).get("/oaiso", c: 401)
+      log.rewind
+      expect(log.read).to match(/\bWARN\b.+\bHTTP 401\b/)
+    end
 
-    connection(logger).get("/oaiso", c: 301)
-    log.rewind
-    expect(log.read).to match(/\bINFO\b.+\bHTTP 301\b/)
-  end
+    it "logs a 5XX response status code at an WARN level" do
+      connection(logger).get("/oaiso", c: 500)
+      log.rewind
+      expect(log.read).to match(/\bWARN\b.+\bHTTP 500\b/)
+    end
 
-  it "logs a 4XX response status code at a WARN level" do
-    logger = Logger.new(log = StringIO.new)
-
-    connection(logger).get("/oaiso", c: 401)
-    log.rewind
-    expect(log.read).to match(/\bWARN\b.+\bHTTP 401\b/)
-  end
-
-  it "logs a 5XX response status code at an WARN level" do
-    logger = Logger.new(log = StringIO.new)
-
-    connection(logger).get("/oaiso", c: 500)
-    log.rewind
-    expect(log.read).to match(/\bWARN\b.+\bHTTP 500\b/)
-  end
-
-  it "logs a cURL-like response package at a DEBUG level" do
-    logger = Logger.new(log = StringIO.new)
-
-    connection(logger).post("/nigirizushi")
-    log.rewind
-    curl = <<-CURL.strip
+    it "logs the response headers and body at a DEBUG level" do
+      connection(logger).post("/nigirizushi")
+      log.rewind
+      curl = <<-CURL.strip
 Content-Type: application/json
 
 {"id":"1"}
 CURL
-    expect(log.read).to match(/\bDEBUG\b.+#{Regexp.escape(curl.inspect)}/)
-  end
+      expect(log.read).to match(/\bDEBUG\b.+#{Regexp.escape(curl.inspect)}/)
+    end
 
-  it "logs errors which occur during the request and re-raises them through" do
-    logger = Logger.new(log = StringIO.new)
+    it "logs errors which occur during the request and re-raises them" do
+      logger = Logger.new(log = StringIO.new)
 
-    expect do
-      connection(logger).get("/error")
-    end.to raise_error(TestError, "An error occurred during the request")
+      expect do
+        connection(logger).get("/error")
+      end.to raise_error(TestError, "An error occurred during the request")
 
-    log.rewind
-    expect(log.read).to match(
-      /\bERROR\b.+\bTestError - An error occurred during the request \(.+\)$/
-    )
+      log.rewind
+      expect(log.read).to match(
+        /\bERROR\b.+\bTestError - An error occurred during the request \(.+\)$/
+      )
+    end
   end
 
   private
